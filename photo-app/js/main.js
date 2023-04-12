e281b727e6b7825ce6c02f4fa1810f6e6820c854
jfennimo <jfennimo@unca.edu>
2023-04-11 22:44:09 -0400

    HW 4 FINALLY done

--- photo-app/js/main.js
+++ photo-app/js/main.js
@@ -5,17 +5,18 @@
 /* Global Variables */
 /********************/
 // import { getAccessToken } from "./utilities";
+
 let token;
 const rootURL = 'https://photo-app-secured.herokuapp.com';
-const username = 'joel';
-const password = 'joel_password';
+const username = "joel";
+const password = "joel_password";
 
-async function getAccessToken (rootURL, username, password) {
+async function getAccessToken(rootURL, username, password) {
     const postData = {
         "username": username,
         "password": password
     };
-    const endpoint = `${rootURL}/api/token/`;
+    const endpoint = `${rootURL}/api/token`;
     const response = await fetch(endpoint, {
         method: "POST",
         headers: {
@@ -31,23 +32,55 @@ async function getAccessToken (rootURL, username, password) {
 /* Your Functions */
 /******************/
 
+// yayy got the image formatting fixed
 const showUser = async () => {
     const endpoint = `${rootURL}/api/profile`;
     const response = await fetch(endpoint, {
         headers: {
             'Content-Type': 'application/json',
-            'Authorization': 'Bearer' + token
+            'Authorization': 'Bearer ' + token
+        }
+    });
+    const data = await response.json();
+    console.log('Profile:', data);
+    document.querySelector('.user').insertAdjacentHTML('beforeend',
+        `<div>
+    <img src="${data.image_url}">
+    <h2>${data.username}</h2>
+    </div>`)
+
+    // `<img src="https://picsum.photos/60/60?q=11" class="pic" />
+    // <h2>gibsonjack</h2>`
+}
+
+const showSuggestions = async (token) => {
+    const endpoint = `${rootURL}/api/suggestions/`;
+    const response = await fetch(endpoint, {
+        method: "GET",
+        headers: {
+            'Content-Type': 'application/json',
+            'Authorization': 'Bearer ' + token
         }
     })
     const data = await response.json();
-    console.log(data);
+    console.log('Suggestions: ', data);
+    const htmlChunk = data.map(suggestionToHtml).join('')
+    document.querySelector('.suggestions').innerHTML = htmlChunk;
+}
 
-    document.querySelector("#user").insertAdjacentHTML('beforeend',
-    `<div>
-    <img src="${data.image_url}">
-    <h2>${data.username}</h2>
-    </div>`);
-    
+const suggestionToHtml = (suggestion) => {
+    return `
+    <section id="suggestion">
+    <img src="${suggestion.image_url}" class="pic" alt="${suggestion.username}">
+        <div>
+            <p>${suggestion.username}</p>
+            <p>suggested for you</p>
+            </div>
+            <div>
+                <button role="switch" class="link following" aria-checked="false"
+                    aria-label="Follow ${suggestion.username}">follow</button>
+            </div>
+    </section>`
 }
 
 const showStories = async () => {
@@ -60,15 +93,15 @@ const showStories = async () => {
     })
     const data = await response.json();
     console.log(data);
-    const htmlChunk = data.map(storyToHtml).join('');
-    document.querySelector('#stories').innerHTML = htmlChunk;
+    const htmlString = data.map(storyToHtml).join('');
+    document.querySelector('.stories').innerHTML = htmlString;
 }
 
 const storyToHtml = story => {
-    return `<section class="story">
-        <img src="${story.user.thumb_url}" />
+    return `<div>
+        <img src="${story.user.thumb_url}" class="pic"/>
         <p>${story.user.username}</p>
-    </section>
+        </div>
     `
 }
 
@@ -87,118 +120,189 @@ const showPosts = async () => {
     const data = await response.json();
     console.log('Posts:', data);
 
-    const arrayOfHTML = data.map(postToHTML);
+    const arrayOfHTML = data.map(postToHtml);
     const htmlString = arrayOfHTML.join('');
-    document.querySelector('#card').innerHTML = htmlString;
+    document.querySelector('.card').innerHTML = htmlString;
+}
+
+const getLikeButton = post => {
+    if(post.current_user_like_id) {
+        return `
+        <button class="icon-button" onclick="unlikePost(${post.current_user_like_id}, ${post.id})">
+            <i class="fas fa-heart"></i>
+        </button>
+        `;
+    } else {
+        return `
+        <button class="icon-button" onclick="likePost(${post.id})">
+            <i class="far fa-heart"></i>
+        </button>
+        `;
+    }
 }
 
 const getBookmarkButton = post => {
     if (post.current_user_bookmark_id) {
         return `
-        <button onclick="unbookmarkPost(${post.current_user_bookmark_id}, ${post.id})">
+        <button class="icon-button" onclick="unbookmarkPost(${post.current_user_bookmark_id}, ${post.id})">
             <i class="fa-solid fa-bookmark"></i>
         </button>
         `;
     } else {
         return `
-        <button onclick="createBookmark(${post.id})">
+        <button class="icon-button" onclick="createBookmark(${post.id})">
             <i class="fa-regular fa-bookmark"></i>
         </button>
         `;
     }
 }
 
-const requeryRedraw = async (postID) => {
-    const endpoint = `${rootURL}/api/posts/${postID}`;
-    const response = await fetch(endpoint, {
-        headers: {
-            'Content-Type': 'application/json',
-            'Authorization': 'Bearer ' + token
-        }
-    })
-    const data = await response.json();
-    console.log(data);
-    const htmlString = postToHTML(data);
-    targetElementAndReplace(`#post_${postID}`, htmlString);
-}
+const postToHtml = post => {
 
-const createBookmark = async (postID) => {
-    // define the endpoint:
-    const endpoint = `${rootURL}/api/bookmarks/`;
-    const postData = {
-        "post_id": postID
-    };
-
-    // Create the bookmark:
-    const response = await fetch(endpoint, {
-        method: "POST",
-        headers: {
-            'Content-Type': 'application/json',
-            'Authorization': 'Bearer ' + token
-        },
-        body: JSON.stringify(postData)
-    })
-    const data = await response.json();
-    console.log(data);
-    requeryRedraw(postID);
-}
-
-const unbookmarkPost = async (bookmarkId, postID) => {
-    // define the endpoint:
-    const endpoint = `${rootURL}/api/bookmarks/${bookmarkId}`;
-
-    // Create the bookmark:
-    const response = await fetch(endpoint, {
-        method: "DELETE",
-        headers: {
-            'Content-Type': 'application/json',
-            'Authorization': 'Bearer ' + token
-        }
-    })
-    const data = await response.json();
-    console.log(data);
-    requeryRedraw(postID);
-}
-
-const postToHTML = post => {
-    // console.log(post.comments.length);
-    return `
-        <section id="post_${post.id}" class="post">
-            <img src="${post.image_url}" alt="Fake image" />
-
-            ${getBookmarkButton(post)}
-
-            <p>${post.caption}</p>
-            ${showCommentAndButtonIfItMakesSense(post)}
-        </section>
-    `
-}
-
-showModal = () => {
-    alert('Show Modal');
-}
-
-const showCommentAndButtonIfItMakesSense = post => {
-    const hasComments = post.comments.length > 0;
-    const lastCommentIndex = post.comments.length - 1;
-    if (hasComments) {
-        return `<div>
-            <button onclick="showModal()">View all ${post.comments.length} comments</button>
-            <p>${post.comments[lastCommentIndex].text}</p>
-        </div>`;
+    const numComments = post.comments.length;
+    const newestComment = post.comments[numComments - 1];
+    
+    if(numComments > 1) {
+        showComments = `<section id="view-all-comments" onclick="openModal(${post.id})"> <button class="button">View all ${numComments} comments</button></section>
+        <p>
+            <strong>${newestComment.user.username}</strong> 
+            ${newestComment.text}
+        </p>`;
     } else {
-        return '';
+        showComments = `<p>
+            <strong>${newestComment.user.username}</strong> 
+            ${newestComment.text}
+        </p>
+        `
     }
+
+    return `
+    <div class="header">
+    <h3>${post.user.username}</h3>
+    <button class="icon-button"><i class="fas fa-ellipsis-h"></i></button>
+</div>
+<img src="${post.image_url}" alt="sigh" width="300" height="300">
+<div class="info">
+    <div class="buttons">
+        <div>
+            ${getLikeButton(post)}
+            <button class="icon-button"><i class="far fa-comment"></i></button>
+            <button class="icon-button"><i class="far fa-paper-plane"></i></button>
+        </div>
+        <div>
+            ${getBookmarkButton(post)}
+        </div>
+    </div>
+    <p class="likes"><strong>${post.likes.length} likes</strong></p>
+    <div class="caption">
+        <p>
+            <strong>${post.user.username}</strong> 
+            ${post.caption}
+        </p>
+    </div>
+    <div class="comments">
+        ${showComments}
+        <p class="timestamp">${post.display_time}</p>
+    </div>
+</div>
+<div class="add-comment">
+    <div class="input-holder">
+        <i class="far fa-smile"></i>
+        <input type="text" placeholder="Add a comment...">
+    </div>
+    <button class="button">Post</button>
+</div>
+`
 }
 
-/**
- * Helper function to replace a DOM element.
- * https://developer.mozilla.org/en-US/docs/Web/API/Node/replaceChild
- * 
- *  Arguments: 
- *     1. selector: the selector you want to target (string)
- *     2. newHTML:  the HTML you want to replace
- */
+const openModal = (id) => {
+    const modalElement = document.querySelector('.modal-bg');
+
+    console.log('pls work');
+    // shows the modal:
+    modalElement.classList.remove('hidden');
+
+    // accessibility:
+    modalElement.setAttribute('aria-hidden', 'false');
+
+    // puts the focus on the "close" button:
+    document.querySelector('.close').focus();
+
+    modalInfo(id);
+}
+
+const closeModal = () => {
+    const modalElement = document.querySelector('.modal-bg');
+
+    // hides the modal:
+    modalElement.classList.add('hidden');
+
+    // accessibility:
+    modalElement.setAttribute('aria-hidden', 'false');
+}
+
+const modalInfo = async (id) => {
+    // UGHASDUFH RIGHT HERE
+    const endpoint = `${rootURL}/api/posts/${id}`;
+    const response = await fetch(endpoint, {
+        method: "GET",
+        headers: {
+            'Content-Type': 'application/json',
+            'Authorization': 'Bearer ' + token
+        }
+    })
+    const data = await response.json();
+    console.log('Posts:', data);
+
+    document.querySelector('.modal-body').innerHTML = 
+    `<img class="fixed" src="${data.image_url}" />
+    <section class="the-comments">
+        <div class="top">
+            <img src="${data.user.image_url}">
+            <strong>${data.user.username}</strong>
+            <p>
+                ${data.caption}
+            </p>
+            <p class="timestamp">${data.display_time}</p>
+        </div>
+        <div class="row">
+            <strong>${data.user.username}</strong>
+            
+        </div>
+    </section>`;
+
+    document.querySelector(".row").innerHTML = '';
+    data.comments.forEach(modalComments);
+}
+
+const modalComments = (data) => {
+    document.querySelector('.row').insertAdjacentHTML('beforeend',
+    `
+    <div>
+    <p>
+        <strong>${data.user.username}</strong> 
+        ${data.text}
+    </p>
+    <p class="timestamp">${data.display_time}</p>
+    </div>
+    `
+    )
+}
+
+// function ensures that if the tabbing gets to the end of the 
+// modal, it will loop back up to the beginning of the modal:
+document.addEventListener('focus', function(event) {
+
+    const modalElement = document.querySelector('.modal-bg');
+    console.log('focus');
+    if (modalElement.getAttribute('aria-hidden') === 'false' && !modalElement.contains(event.target)) {
+        console.log('back to top!');
+        event.stopPropagation();
+        document.querySelector('.close').focus();
+    }
+}, true);
+
+
 const targetElementAndReplace = (selector, newHTML) => {
     const div = document.createElement('div');
     div.innerHTML = newHTML;
@@ -215,12 +319,9 @@ const initPage = async () => {
 
     // then use the access token provided to access data on the user's behalf
     showUser(token);
-    // showSuggestions(token);
-    // showStories(token);
-    // showPosts(token);
-
-    // query for the user's profile
-    // query for suggestions
+    showSuggestions(token);
+    showStories(token);
+    showPosts(token);
 }
 
 /******************************************/

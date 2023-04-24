fce541b5b9eca0d2f957bf2946aa55a3c5e5fb2d
jfennimo <jfennimo@unca.edu>
2023-04-24 02:36:25 -0400

    all tests passing - hw8

--- hw07/views/following.py
+++ hw07/views/following.py
@@ -1,7 +1,8 @@
 from flask import Response, request
 from flask_restful import Resource
-from models import Following, User, db
+from models import db, Following, User
 import json
+from views import get_authorized_user_ids
 
 def get_path():
     return request.host_url + 'api/posts/'
@@ -19,7 +20,28 @@ class FollowingListEndpoint(Resource):
         # create a new "following" record based on the data posted in the body 
         body = request.get_json()
         print(body)
-        return Response(json.dumps({}), mimetype="application/json", status=201)
+
+        try:
+            following_id = int(body.get('user_id'))
+        except:
+            return Response(json.dumps({'error': 'user_id format incorrect'}), status=400) 
+        
+        user = User.query.get(following_id)
+    
+        if user == None:
+            return Response(json.dumps({ "error": "user_id not valid" }), mimetype="application/json", status=404)
+
+        following = Following(
+            user_id=self.current_user.id,
+            following_id=body.get('user_id')
+        )
+        try:
+            db.session.add(following)
+            db.session.commit()
+        except:
+            return Response(json.dumps({ "error": "duplicate user_id" }), mimetype="application/json", status=400)
+    
+        return Response(json.dumps(following.to_dict_following()), mimetype="application/json", status=201)
 
 class FollowingDetailEndpoint(Resource):
     def __init__(self, current_user):
@@ -28,7 +50,16 @@ class FollowingDetailEndpoint(Resource):
     def delete(self, id):
         # delete "following" record where "id"=id
         print(id)
-        return Response(json.dumps({}), mimetype="application/json", status=200)
+
+        following = Following.query.get(id)
+    
+        if following == None or following.user_id != self.current_user.id:
+            return Response(json.dumps({ "error": "follow_user_id not valid or is unauthorized" }), mimetype="application/json", status=404)
+
+        Following.query.filter_by(id=id).delete()
+        db.session.commit()
+
+        return Response(json.dumps(None), mimetype="application/json", status=200)
 
 
 
